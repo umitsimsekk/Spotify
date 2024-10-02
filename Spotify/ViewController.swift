182fcbcb98acbd809a2603f803f0f1b82a27ce39
Ümit Şimşek <umitsimsek002@gmail.com>
2024-10-02 22:25:35 +0300

    spotify first commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Ümit Şimşek on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        title = "Main Page"
+    }
+
+
+}
+
